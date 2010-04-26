88d6e3b86477aaf3830fc101495fe0599f04d4c3
Graham Ashton <graham@effectif.com>
2010-04-26 22:53:23 +0100

    Started iterating over a product's topics.

--- satisfaction.py
+++ satisfaction.py
@@ -10,8 +10,8 @@ class ResourceNotFound(RuntimeError): pass
 class Parser:
     
     def __init__(self, url):
-        self._document = None
         self.url = url
+        self._document = None
 
     @property
     def document(self):
@@ -24,15 +24,25 @@ class Parser:
         raise ResourceNotFound()
 
 
+class HtmlParser(Parser):
+
+    def load_document(self):
+        response = urllib.urlopen(self.url)
+        if response.headers.getheader('status') == '404':
+            self.resource_not_found()
+        self._document = lxml.html.document_fromstring(response.read())
+
+
 class AtomParser(Parser):
     
-    def __init__(self, *args):
-        Parser.__init__(self, *args)
+    def __init__(self, url, skip_first_entry=False):
+        Parser.__init__(self, url)
+        self.first_child_entry = 1 if skip_first_entry else 0
         self.page = 1
     
     def __iter__(self):
         while True:
-            for entry in self.document.entries[1:]:
+            for entry in self.document.entries[self.first_child_entry:]:
                 yield entry
             if self.more_pages_to_load():
                 self.load_next_page()
@@ -52,33 +62,27 @@ class AtomParser(Parser):
         self.page += 1
     
     @staticmethod
-    def url_for_page(url, page):
-        return url + '?page=%s' % page
+    def url_for_page(page):
+        return self.url + '?page=%s' % page
     
     def load_document(self):
-        document = feedparser.parse(self.url_for_page(self.url, self.page))
+        document = feedparser.parse(self.url_for_page(self.page))
         if document.get('status', None) == 404:
             self.resource_not_found()
         self._document = document
 
-
-class HtmlParser(Parser):
-    
-    def load_document(self):
-        response = urllib.urlopen(self.url)
-        if response.headers.getheader('status') == '404':
-            self.resource_not_found()
-        self._document = lxml.html.document_fromstring(response.read())
-    
 
 class Resource:
     
     def __init__(self, resource_id):
         self.resource_id = resource_id
     
-    @property
     def url(self):
         return self.URL % {'id': self.resource_id}
+    
+    def child_url(self, resource):
+        print '%s/%s' % (self.url, resource)
+        return '%s/%s' % (self.url, resource)
     
     @property
     def document(self):
@@ -91,24 +95,35 @@ class Product(Resource):
 
     def __init__(self, *args):
         Resource.__init__(self, *args)
-        self.parser = HtmlParser(self.url)
+        self.parser = HtmlParser(self.url())
+        self._topic_parser = None
 
     @property
     def title(self):
         return self.document.cssselect('a.name')[0].text_content()
     
-    # @property
-    # def topics(self):
-    #     return iter(AtomParser(self.url + "/topics"))
+    @property
+    def topic_parser(self):
+        if self._topic_parser is None:
+            self._topic_parser = AtomParser(self.child_url('topics'))
+        return self._topic_parser
+
+    @property
+    def topic_count(self):
+        return int(self.topic_parser.document.feed['totalresults'])
+
+    @property
+    def topics(self):
+        return iter(self.topic_parser)
 
 
 class Topic(Resource):
     
     URL = 'http://api.getsatisfaction.com/topics/%(id)s'
     
-    def __init__(self, *args):
-        Resource.__init__(self, *args)
-        self.parser = AtomParser(self.url)
+    def __init__(self, resource_id):
+        Resource.__init__(self, resource_id)
+        self.parser = AtomParser(self.url(), skip_first_entry=True)
     
     @property
     def title(self):
--- satisfaction_test.py
+++ satisfaction_test.py
@@ -1,48 +1,74 @@
 import os
 import unittest2 as unittest
 
+import mock
+
 import satisfaction
+
+
+def urlopen_404(url):
+    headers = mock.Mock()
+    headers.getheader.return_value = '404'
+    response = mock.Mock()
+    response.headers = headers
+    return response
+    
+
+def feedparser_404(url):
+    return {'status': 404}
 
 
 class TestHelper(unittest.TestCase):
     
+    def stub_method(self, cls, name, stub):
+        original = getattr(cls, name)
+        self.original_methods.append((cls, original))
+        setattr(cls, name, stub)
+    
     def setUp(self):
+        self.original_urls = []
+        self.original_methods = []
         if hasattr(self, 'withFixtures'):
-            self.original_urls = []
             self.withFixtures()
+
+        if len(self.original_methods) == 0:
+            def url_for_page(self, page):
+                return '%s-page-%s.xml' % (self.url.rsplit('.', 1)[0], page)
+            self.stub_method(satisfaction.AtomParser, 'url_for_page', url_for_page)
+        
+            def child_url(self, resource):
+                return '%s-%s.xml' % (self.url().rsplit('.', 1)[0], resource)
+            self.stub_method(satisfaction.Resource, 'child_url', child_url)
     
     def tearDown(self):
         if hasattr(self, 'real_atom_url_func'):
             satisfaction.AtomParser.url_for_page = self.real_atom_url_func
+        if hasattr(self, 'original_methods'):
+            for cls, method in self.original_methods:
+                setattr(cls, method.__name__, method)
         if hasattr(self, 'original_urls'):
-            for cls, func in self.original_urls:
-                cls.URL = func
+            for cls, url in self.original_urls:
+                cls.URL = url
     
     def useFixture(self, stubbed_cls, name=None):
-        if not hasattr(self, 'real_atom_url_func'):
-            def stub_url_func(self, url, page):
-                return '%s-page-%s.xml' % (url.rsplit('.', 1)[0], page)
-            self.real_atom_url_func = satisfaction.AtomParser.url_for_page
-            satisfaction.AtomParser.url_for_page = stub_url_func
-
         self.original_urls.append((stubbed_cls, stubbed_cls.URL))
         base = stubbed_cls.__name__.lower()
         filename = '%s-%s.xml' % (base, name) if name else '%s.html' % base
         stubbed_cls.URL = os.path.join(os.getcwd(), 'fixtures', filename)
-
+    
     def product(self):
         return satisfaction.Product('1234')
 
     def topic(self):
-        # TODO: topic id is ignored in tests - does it work?
         return satisfaction.Topic('1234')
 
 
 class MissingProductTest(TestHelper):
     
+    @mock.patch('urllib.urlopen', urlopen_404)
     def test_product_not_found(self):
         with self.assertRaises(satisfaction.ResourceNotFound):
-            satisfaction.Product('bad_product_name').title
+            self.product().title
 
 
 class ProductWithTopicsTest(TestHelper):
@@ -53,13 +79,16 @@ class ProductWithTopicsTest(TestHelper):
     def test_can_retrieve_title(self):
         self.assertEqual('Wordtracker Keywords', self.product().title)
     
-    @unittest.skip("pending url refactoring")
+    @unittest.skip('pending instantiation of topic from data')
     def test_can_retrieve_topics(self):
+        self.assertEqual(3, self.product().topic_count)
         self.assertEqual(3, len(list(self.product().topics)))
+        self.assertIsInstance(list(self.product().topics)[0], satisfaction.Topic)
 
 
 class MissingTopicTest(TestHelper):
 
+    @mock.patch('feedparser.parse', feedparser_404)
     def test_topic_not_found(self):
         with self.assertRaises(satisfaction.ResourceNotFound):
             self.topic().title
